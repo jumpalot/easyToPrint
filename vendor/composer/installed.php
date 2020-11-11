295a9cba3fd7549a2f819810be35470560264619
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-11 01:31:02 -0300

    added composer + bootstrap

---
+++ vendor/composer/installed.php
@@ -0,0 +1,40 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4b3498cf753d9ef5a55b804c318d030a9bf902e8',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4b3498cf753d9ef5a55b804c318d030a9bf902e8',
+    ),
+    'twbs/bootstrap' => 
+    array (
+      'pretty_version' => 'v4.5.3',
+      'version' => '4.5.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a716fb03f965dc0846df479e14388b1b4b93d7ce',
+    ),
+    'twitter/bootstrap' => 
+    array (
+      'replaced' => 
+      array (
+        0 => 'v4.5.3',
+      ),
+    ),
+  ),
+);

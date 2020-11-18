a2173719afe822614ce090830d391c233c7ff6ef
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-18 17:05:45 -0300

    eliminado parametro carnet por get

--- getFoto.php
+++ getFoto.php
@@ -1,6 +1,6 @@
 <?php
     include('DBCon.php');
-    $carnet = (isset($_POST['carnet'])) ? $_POST['carnet'] : $_GET['carnet'];
+    $carnet = $_POST['carnet'];
     $sql = "SELECT Miniatura FROM alumnos WHERE Carnet='$carnet'";
     $res = $db->query($sql);
     if ($db->error!="") echo $db->error;

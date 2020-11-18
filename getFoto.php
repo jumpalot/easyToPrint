368aa0f7e48a9a0f0915383d9f631c6b8d8273aa
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-18 17:04:44 -0300

    notificar error si lo hay en getFoto

--- getFoto.php
+++ getFoto.php
@@ -2,7 +2,8 @@
     include('DBCon.php');
     $carnet = (isset($_POST['carnet'])) ? $_POST['carnet'] : $_GET['carnet'];
     $sql = "SELECT Miniatura FROM alumnos WHERE Carnet='$carnet'";
-    $aux = array();
-    echo $db->query($sql)->fetch_array()[0];
+    $res = $db->query($sql);
+    if ($db->error!="") echo $db->error;
+    else echo $res->fetch_array()[0];
     mysqli_close($db);
 ?> 

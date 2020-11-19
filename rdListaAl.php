f1d1a438e420b8ec04a5653c668444015e847e05
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-19 18:02:54 -0300

    remover y reducir chips separados

--- rdListaAl.php
+++ rdListaAl.php
@@ -2,7 +2,7 @@
     include('DBCon.php');
     $carnet = $_POST['carnet'];
     $idl = $_POST['idl'];
-    $sql = "DELETE FROM alumno_lista WHERE idLista=$idl AND Carnet=$carnet";
+    $sql = "UPDATE alumno_lista SET cantidad=cantidad-1 WHERE Carnet='$carnet' AND idLista='$idl'";
     $db->query($sql);
     echo $db->error;
     mysqli_close($db);

8439dfd317b16b1f02bcd19992883fd24cc7d8ab
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-19 17:45:41 -0300

    merge fix

--- addListaAl.php
+++ addListaAl.php
@@ -3,11 +3,7 @@
     $carnet = $_POST['carnet'];
     $idl = $_POST['idl'];
     $sql = "INSERT INTO alumno_lista(Carnet, idLista)
-<<<<<<< HEAD
             VALUES ('$carnet', '$idl') 
-=======
-            VALUES ($carnet, $idl) 
->>>>>>> 035c65ee75bc679e6f4c32f1f1cb07800515d0b5
             ON DUPLICATE KEY UPDATE cantidad=cantidad+1";
     echo $db->error;
     mysqli_close($db);

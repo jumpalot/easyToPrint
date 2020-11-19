3e41e51f47ddb17d926a28a48c48956807c63555
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-19 17:32:59 -0300

    eliminar chip terminado

--- rmListaAl.php
+++ rmListaAl.php
@@ -2,10 +2,8 @@
     include('DBCon.php');
     $carnet = $_POST['carnet'];
     $idl = $_POST['idl'];
-    $sql = "DELETE FROM alumno_lista WHERE idLista=$idl AND Carnet=$carnet AND cantidad=1;
-    INSERT INTO alumno_lista(Carnet, idLista)
-            VALUES ($carnet, $idl) 
-            ON DUPLICATE KEY cantidad=cantidad+1";
+    $sql = "DELETE FROM alumno_lista WHERE idLista=$idl AND Carnet=$carnet AND cantidad=$idl";
+    $db->query($sql);
     echo $db->error;
     mysqli_close($db);
 ?>

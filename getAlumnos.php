a486332abf5a82a65b63527d49ceaea63d457dae
frankxjx8 <74795426+frankxjx8@users.noreply.github.com>
2020-11-22 17:22:13 -0300

    Update getAlumnos.php

--- getAlumnos.php
+++ getAlumnos.php
@@ -1,23 +1,11 @@
 <?php
-   /* include('DBCon.php');
+    include('DBCon.php');
     $sql = "SELECT Carnet, Curso, Division, Nombre, Apellido, hasImg, Dni, Turno 
             FROM alumnos NATURAL JOIN turnos";
     $aux = array();
     foreach($db->query($sql) as $alumno)
         array_push($aux, $alumno);
     echo json_encode($aux);
-    mysqli_close($db);*/
-use RebaseData\InputFile\InputFile;
-use RebaseData\Converter\Converter;
+    mysqli_close($db);
 
-$inputFile = new InputFile('biblioteca2.mdb');
-$inputFiles = [$inputFile];
-
-$converter = new Converter();
-$database = $converter->convertToDatabase($inputFiles);
-$table = $database->getTable('alumnos');
-
-foreach ($table->getColumns() as $column) {
-    echo "Got column: ".$column->getName()."\n";
-}
 ?>

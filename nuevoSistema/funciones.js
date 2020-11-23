24c69f29c0bd930e95dc796282c084c17c0c5ca1
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-23 14:48:35 -0300

    ahora las divisiones no se multiplicaran con cada cambio

--- nuevoSistema/funciones.js
+++ nuevoSistema/funciones.js
@@ -1,4 +1,6 @@
+var divisiones;
 window.onload = () => {
+    divisiones = $('#divisiones').html();
     $.post("../getCursos.php", (cursos) => {
         var lista = $('#cursos').html();
         cursos.split(',').forEach( (curso) => 
@@ -10,8 +12,8 @@ window.onload = () => {
 function actualizarDivis(curso){
     if (curso!="")
         $.post("../getDivisiones.php", {curso: curso}, (divisiones) => {
-            var lista = $('#divisiones').html();
-            divisiones.split(',').forEach( (division) => 
+            var lista = divisiones;
+            lista.split(',').forEach( (division) => 
                 lista += '<option value="' + division + '">' + division + 'ª</option>'
             );
             $('#divisiones').html(lista);

3c974be15d23e4bb506d6c2b3e9875db68fc2763
Mimirko <emirorellana18@gmail.com>
2020-11-22 21:06:09 -0300

    primer intento filtrado curso division

--- getAlumno.php
+++ getAlumno.php
@@ -3,6 +3,18 @@
     $sql = "SELECT Carnet, Curso, Division, Nombre, Apellido, hasImg, Dni, Turno 
             FROM alumnos NATURAL JOIN turnos
             WHERE Carnet=".$_GET['carnet'];
+            if(isset($_GET['curso']){
+                $curso=$_GET['curso'];
+                $sql.=" WHERE Curso='$curso'";
+                if(isset($_GET['division']){
+                    $division=$_GET['division'];
+                    $sql.=" AND Curso='$division'"
+                }
+            }      
+            else if(isset($_GET['division']){
+                    $division$_GET['division'];
+                    $sql.=" WHERE Division='$division'"
+                    }
     $aux = array();
     foreach($db->query($sql) as $alumno)
         array_push($aux, $alumno);
--- nuevoSistema/main.php
+++ nuevoSistema/main.php
@@ -4,23 +4,31 @@
         <div class="card card-signin my-5">
           <div class="card-body">
             <h5 class="card-title text-center h2">Easy Carnet</h5>
-            <form class="form-signin" method="GET" action=".">
+            <form class="form-signin" method="GET" action="../getAlumno.php">
               <br>
               <div class="form-label-group Collapse">
                 <select name="curso" id="cursos" class="form-control"  required autofocus>
                   <option selected disabled hidden>Ingrese Curso</option>
-                	<option>Primero</option>
-                	<option>Segundo</option>
-                	<option>Tercero</option>
+                	<option value="1">Primero</option>
+                	<option value="2">Segundo</option>
+                  <option value="3">Tercero</option>
+                  <option value="4">Cuarto</option>
+                  <option value="5">Quinto</option>
+                  <option value="6">Sexto</option>
+                  <option value="7">Septimo</option>
                 </select>
               </div>
               <br>
               <div class="form-label-group Collapse">
                 <select name="division" id="divisiones" class="form-control " required>
                   <option selected disabled hidden>Ingrese Division</option>
-                	<option>Primera</option>
-                	<option>Segunda</option>
-                	<option>Tercera</option>
+                	<option value="1">Primera</option>
+                	<option value="2">Segunda</option>
+                  <option value="3">Tercera</option>
+                  <option value="4">Cuarta</option>
+                  <option value="5">Quinta</option>
+                  <option value="6">Sexta</option>
+                  
                 </select>
               </div>
               <br>

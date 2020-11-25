b4111724423b24938ddff8354588bf832689811c
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-25 14:51:56 -0300

    js y view

--- nuevoSistema/index.php
+++ nuevoSistema/index.php
@@ -5,30 +5,30 @@
     switch(@$_GET['page']){
         case 'list':
             $alumnos = getAlumnos($_GET['curso'], $_GET['division']);
-            include('simpleheader.html');
+            include('view/simpleheader.html');
             foreach($alumnos as $alumno){
                 if($alumno->hasImg){
                     include('bindAlumno.php');
                     include('plantilla.php');
                 }
             }
-            include('autoprint.html');
+            include('js/autoprint.html');
             break;
         case 'reverse':
             $alumnos = getAlumnos($_GET['curso'], $_GET['division']);
-            include('simpleheader.html');
+            include('view/simpleheader.html');
             foreach($alumnos as $alumno){
                 if($alumno->hasImg){
                     $carnet=$alumno->Carnet;
                     include('parteAtras.php');
                 }
             }
-            include('autoprint.html');
+            include('js/autoprint.html');
             break;  
         default:
-            include('header.html');
-            include('main.php');
-            include('importador.html');
+            include('view/header.html');
+            include('view/main.html');
+            include('view/importador.html');
     }
-    require('footer.html');
+    require('view/footer.html');
 ?>

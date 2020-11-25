8e457495c553c841a07fb628f21014cacfe1317c
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-25 17:43:01 -0300

    tamaño qr

--- nuevoSistema/tpl/parteAtras.php
+++ nuevoSistema/tpl/parteAtras.php
@@ -1,6 +1,10 @@
 <article class="carnet atras">
     <div id="qrcode#<?=$carnet?>" class="qr"></div>
     <script type="text/javascript">
-      new QRCode("qrcode#<?=$carnet?>", "<?=$carnet?>");
+      new QRCode("qrcode#<?=$carnet?>", {
+        text: "<?=$carnet?>",
+        width: 150,
+        height: 150
+      });
     </script>
 </article>

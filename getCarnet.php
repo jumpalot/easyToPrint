680f18262b80f6e4774984d7840ec4edbc6dae21
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-25 03:46:30 -0300

    array cambiado a asociativo. eliminados elementos residuales.

--- getCarnet.php
+++ getCarnet.php
@@ -3,9 +3,8 @@
     $sql = "SELECT Carnet
             FROM alumnos
             WHERE Dni=".$_POST['dni'];
-    $aux = array();
     $res = $db->query($sql);
-    if ($db->error="") echo $res->fetch_array()[0];
-    echo "0";
+    if ($db->error="") echo $res->fetch_array()["Carnet"];
+    else echo "0";
     mysqli_close($db);
 ?>

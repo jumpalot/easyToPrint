7b23868285d8b6ce998960937d67a4839d9ba83b
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-25 04:01:30 -0300

    sintaxis error

--- getCarnet.php
+++ getCarnet.php
@@ -3,7 +3,7 @@
     $dni = $_REQUEST['dni'];
     $sql = "SELECT Carnet FROM alumnos WHERE Dni='$dni'";
     $res = $db->query($sql);
-    if ($db->error="") echo $res->fetch_array(MYSQLI_ASSOC)["Carnet"];
+    if ($db->error=="") echo $res->fetch_array(MYSQLI_ASSOC)["Carnet"];
     else echo "0";
     mysqli_close($db);
 ?>

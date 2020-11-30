e165d89f422dede80a25430e83563843ae144028
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-30 02:11:32 -0300

    missing query fixed

--- addLista.php
+++ addLista.php
@@ -2,6 +2,7 @@
     include('DBCon.php');
     $nom = $_POST['nom'];
     $sql = "INSERT INTO listas (nombre) VALUES ('$nom')";
+    $db->query($sql);
     echo $db->error;
     mysqli_close($db);
 ?>

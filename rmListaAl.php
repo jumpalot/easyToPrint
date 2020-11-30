2a030ddc2cc9d9f155310fda19cf8659386757b1
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-11-30 16:46:39 -0300

    comillas simples faltantes

--- rmListaAl.php
+++ rmListaAl.php
@@ -2,7 +2,7 @@
     include('DBCon.php');
     $carnet = $_POST['carnet'];
     $idl = $_POST['idl'];
-    $sql = "DELETE FROM alumno_lista WHERE idLista=$idl AND Carnet=$carnet";
+    $sql = "DELETE FROM alumno_lista WHERE idLista='$idl' AND Carnet='$carnet'";
     $db->query($sql);
     echo $db->error;
     mysqli_close($db);

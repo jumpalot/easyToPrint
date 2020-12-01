1fbd260cfff205909342e29a7a718b0953cd64a1
Juan Pablo <59149780+jumpalot@users.noreply.github.com>
2020-12-01 12:40:39 -0300

    ahoras admite divisiones no numericas

--- getDivision.php
+++ getDivision.php
@@ -3,7 +3,7 @@
     $cur=$_POST['curso']; $divi=$_POST['division'];
     $sql = "SELECT Carnet, Curso, Division, Nombre, Apellido, hasImg, Dni, Turno 
             FROM alumnos NATURAL JOIN turnos
-            WHERE Curso=$cur AND Division=$divi";
+            WHERE Curso='$cur' AND Division='$divi'";
     $aux = array();
     foreach($db->query($sql) as $alumno)
         array_push($aux, $alumno);
